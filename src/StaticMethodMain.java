a88a62a0ba869f5da31e4ff20ba0fdaa9a20aa2a
Oluwatosin Faith Ogungbaye <hellotosinfaith@gmail.com>
2023-11-16 22:21:55 +0000

    ...

---
+++ src/StaticMethodMain.java
@@ -0,0 +1,24 @@
+public class StaticMethodMain {
+    public static void main(String[] args) {
+        //static
+        //Class method
+        //Dont need instance
+        //Call with class name
+
+        TosinClass.subtraction()
+
+
+
+        //non - static
+        //Object methods
+        //To use - must create an instance of new object
+        TosinClass tosinClass = new TosinClass();
+        tosinClass.sum();
+
+
+
+
+
+
+    }
+}
---
+++ src/TosinClass.java
@@ -0,0 +1,10 @@
+public class TosinClass {
+
+    public int sum(int firstNumber, int secondNumber){
+        return firstNumber + secondNumber;
+    }
+
+    public static int subtraction(int firstNumber, int secondNumber){
+        return firstNumber - secondNumber;
+    }
+}
